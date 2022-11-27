6a32fa6c97db1a481b58486515991943fc3db899
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-11-27 19:51:13 -0300

    Delete Vite Icon, Add Data & Perfil Image

---
+++ src/data/projects.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        "name": "Harry Potter API Rest",
+        "description": "En este proyecto desarrolle una api enfocada al mundo de Harry Potter, inclui en este proyecto la mayor informacion que pude encontrar sobre este mundo",
+        "year": "2022",
+        "url": "https://github.com/DePalma2/api-rest-harry-potter",
+        "technologies": ["Java", "Spring Boot", "MySQL", "JPA", "Spring Security"]
+    },
+    {
+        "name": "Carrito de compras",
+        "description": "En este proyecto podrán agregar productos a un carrito de compras, agregar mas cantidad de productos o disminuirlos también pueden eliminarlos, dentro del menú encontraran una sección de contacto donde podrán enviar mensajes estos se programaron para que solo se vean por consola lo mismo pasa con el checkout de los productos que se encuentra en la sección del carrito.",
+        "year": "2022",
+        "url": "https://github.com/DePalma2/Cart-React",
+        "technologies": ["React JS", "Axios", "Bootstrap", "Formik", "Vite JS"]
+    },
+    {
+        "name": "AlkemyBank",
+        "description": "Proyecto desarrollado para la acceleracion de Alkemy, este proyecto trata de simular un HomeBanking, se pueden realizar multiples operacion desde transferir dinero hasta enviarlo a otros usuarios tambien cuenta con un dashboard para acceder a dichas operaciones",
+        "year": "2022",
+        "url": "https://github.com/AllegriM/Skill-Up-React-G6",
+        "technologies": ["React JS", "Axios", "Tailwind", "React Router"]
+    },
+    {
+        "name": "Ollivander's Market",
+        "description": "En este proyecto se desarrollo una pagina con tematica del mundo de Harry Potter",
+        "year": "2021 - 2022",
+        "url": "https://github.com/DePalma2/ollivanders-market",
+        "technologies": ["HTML", "CSS", "PHP", "JavaScript", "Bootstrap"]
+    }
+]
