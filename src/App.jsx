f616ac2e2050a56d674ada68be8030873ece83cf
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-11-27 19:50:04 -0300

    Add Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Portfolio from './components/Portfolio';
 
 function App() {
 
   return (
     <div className='App'>
       <Header/>
+      <Portfolio/>
     </div>
   )
 }
