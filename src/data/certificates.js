f8a9d240bbf20869aaea4cd7f745ef08c93bbd74
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-20 20:14:13 -0300

    Json Certificates

---
+++ src/data/certificates.js
@@ -0,0 +1,72 @@
+export default [
+    {
+        "name": "React JS",
+        "instituto": "UTN",
+        "image": "https://i.ibb.co/yNKXrCB/Certificado-React-JS.png"
+    },
+    {
+        "name": "Desarrollador Front-End",
+        "instituto": "Da Vinci",
+        "image": "https://i.ibb.co/SPfHQ0Q/Certificado-Da-Vinci-Desarrollador-Front-End.png"
+    },
+    {
+        "name": "Full Stack Java - Codo a Codo",
+        "instituto": "Gobierno de la Ciudad",
+        "image": "https://i.ibb.co/TTCSyLs/Codo-a-Codo-Java.png"
+    },
+    {
+        "name": "React Skill Up",
+        "instituto": "Alkemy",
+        "image": "https://i.ibb.co/hyvv9MM/Skill-Up-Alkemy.png"
+    },
+    {
+        "name": "SQL",
+        "instituto": "OpenBootcamp",
+        "image": "https://i.ibb.co/vjtTcb4/Certificado-Open-Bootcamp-SQL.png"
+    },
+    {
+        "name": "MongoDB Fundamentos",
+        "instituto": "Educacion IT",
+        "image": "https://i.ibb.co/PWT2qtd/Mongo-DB-Fundamentos.png"
+    },
+    {
+        "name": "Paradigma y Objetos",
+        "instituto": "Educacion IT",
+        "image": "https://i.ibb.co/QQ0BkDM/Certificado-Paradigma-y-Obj.png"
+    },
+    {
+        "name": "Introduccion a Bases de Datos",
+        "instituto": "Educacion IT",
+        "image": "https://i.ibb.co/LpzcP6f/Certificado-BD.png"
+    },
+    {
+        "name": "HTML Fundamentos Web",
+        "instituto": "Educacion IT",
+        "image": "https://i.ibb.co/RNjfQGV/Diploma-Html.png"
+    },
+    {
+        "name": "JavaScript Desde Cero",
+        "instituto": "Educacion IT",
+        "image": "https://i.ibb.co/1mZ8kbx/Certificado-Java-Script.png"
+    },
+    {
+        "name": "Introduccion a Seguridad Informatica",
+        "instituto": "Educacion IT",
+        "image": "https://i.ibb.co/jb47Fsb/Certificado-Seg-Inf.png"
+    },
+    {
+        "name": "React Hooks Esenciales",
+        "instituto": "Linkedin",
+        "image": "https://i.ibb.co/6sBDCsP/Linkedin-React-Hooks.png"
+    },
+    {
+        "name": "Adobe XD",
+        "instituto": "Udemy",
+        "image": "https://i.ibb.co/0c957Yp/Certificado-Adobe-XD.jpg"
+    },
+    {
+        "name": "Bootstrap 4",
+        "instituto": "Udemy",
+        "image": "https://i.ibb.co/TKXyXT5/Bootstrap.jpg"
+    }
+]
