da2c05d23659ce4ab1b296c3b397b9f25590cfed
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-20 20:14:45 -0300

    Add footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
 
@@ -8,6 +9,7 @@ function App() {
     <div className='App'>
       <Header/>
       <Portfolio/>
+      <Footer/>
     </div>
   )
 }
