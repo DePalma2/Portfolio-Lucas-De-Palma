f23d62145b58a7ab1db7b130f3541a1e6a7af87d
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-20 20:13:20 -0300

    Add Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <footer class="p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-900">
+            <span class="block text-sm text-white sm:text-center dark:text-gray-400">© 2022 Lucas De Palma   
+            </span>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
