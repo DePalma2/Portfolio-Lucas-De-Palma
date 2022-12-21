853f179aab5babc04aaf15d0be2a7f6e8ab0bf54
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-20 21:38:09 -0300

    Fix footer rounded

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <footer class="p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-900">
+        <footer class="p-4 shadow md:px-6 md:py-8 bg-gray-900">
             <span class="block text-sm text-white sm:text-center dark:text-gray-400">© 2022 Lucas De Palma   
             </span>
         </footer>
