12f2c2c806a70b0df5500a23b024b89772c211ba
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-12-27 20:23:40 -0300

    Change word

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -8,7 +8,7 @@ const Certificates = () => {
   return (
     <div>
         <div>
-            <h2 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-5xl'>Certificados</h2>
+            <h2 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-3xl lg:text-5xl'>Certificaciónes</h2>
         </div>
         <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
         {
